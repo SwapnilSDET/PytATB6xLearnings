924bdf384ff1a1d935afdab9fa34b30d50d88fa4
SwapnilPadekar <slpadekar29@gmail.com>
2025-10-09 14:08:41 +0530

    Adding Day2 programs

---
+++ src/ex_01_Python_Basics/Lab003_print.py
@@ -0,0 +1,27 @@
+print("Hello World!")
+# Output - Hello World!
+
+# print(self, *args, sep=' ', end='\n', file=None):
+# self - please ignore  (will be covered in oops)
+# *args - unlimited number of comma separated arguments.
+
+print("SDETSwapnil", 123, "Amit", "John")
+# Output - SDETSwapnil 123 Amit John
+
+print("SDETSwapnil", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145)
+# Output - SDETSwapnil 123 Amit John Vinod Roznin Ankesh True 3.145
+
+print("SDETSwapnil", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145, sep="-")
+# Output - SDETSwapnil-123-Amit-John-Vinod-Roznin-Ankesh-True-3.145
+
+print("SDETSwapnil", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145, sep="*", end="\n")
+# Output - SDETSwapnil*123*Amit*John*Vinod*Roznin*Ankesh*True*3.145
+
+print("SDETSwapnil", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145, sep="_")
+# Output - SDETSwapnil_123_Amit_John_Vinod_Roznin_Ankesh_True_3.145
+# IndentationError: unexpected indent
+
+print("SDETSwapnil", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145, sep="*", end="\n")
+# Output - SDETSwapnil*123*Amit*John*Vinod*Roznin*Ankesh*True*3.145
+
+# Print() - Case sensitive
