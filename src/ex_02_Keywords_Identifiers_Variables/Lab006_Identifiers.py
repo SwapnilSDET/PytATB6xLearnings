a308847f5f1604144636df8995a965094c7fe294
SwapnilPadekar <slpadekar29@gmail.com>
2025-10-11 08:16:02 +0530

    comments updated in Lab006

--- src/ex_02_Keywords_Identifiers_Variables/Lab006_Identifiers.py
+++ src/ex_02_Keywords_Identifiers_Variables/Lab006_Identifiers.py
@@ -1,13 +1,14 @@
 age = 65
-_age = 65
 print(age)
+
+_age = 65 # Variable starting with _ is allowed.
 print(_age)
 
 _ = 12
-print(_)
+print(_) #12
 _ = _+1
-print(_)
+print(_) #13
 
-#123abc = 90
+#123abc = 90 # Variable starting with numbers is not allowed.
 abc123 = "abc123"
 print(abc123)
